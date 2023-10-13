ccfc76c29d977fce8089213242a074dc1f89d1bf
SoumyajeetMazumdar <soumyajeetmazumdar@gmail.com>
2023-10-13 21:14:58 +0530

    feedback section done, stamp added

--- project-metaverse/sections/Feedback.jsx
+++ project-metaverse/sections/Feedback.jsx
@@ -1,8 +1,63 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col lg:flex-row gap-6`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-solid border-[#6a6a6a] relative"
+      >
+        <div className="feedback-gradient" />
+
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Samantha
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-4 text-white">
+            Founder | Metaversus
+          </p>
+        </div>
+
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[25px] leading-[39px] text-white">
+          With the development of today's technology, metaverse is very useful
+          for today's work. The advent of web 3.0 represents a technological
+          shift, and soon, we will be able to use this tech for just about
+          anything.
+        </p>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="relative flex flex-1 justify-center items-center"
+      >
+        <img
+          src="/planet-09.png"
+          alt="planet"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+
+        <motion.div
+          variants={zoomIn(0.4, 1)}
+          className=" absolute lg:-left-[10%] lg:top-[3%] max-lg:top-[5%] max-lg:left-[5%]"
+        >
+          <img
+            src="/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-[155px] object-contain"
+          />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
