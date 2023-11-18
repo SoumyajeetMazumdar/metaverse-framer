31b363f345748b1332306e8c0cde353e2a41691b
SoumyajeetMazumdar <soumyajeetmazumdar@gmail.com>
2023-11-18 15:00:17 +0530

    eslint removed - deployment issues, build success

--- project-metaverse/next.config.js
+++ project-metaverse/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
